c316b3ad276772504b5092b75570508bb7bfcee0
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-13 20:13:19 +0500

    Task1

---
+++ Tyuiu.RagozinaAD.Sprint3.Task1.V13.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RagozinaAD.Sprint3.Task1.V13.Lib;
+namespace Tyuiu.RagozinaAD.Sprint3.Task1.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 1.2;
+            int startValue = 1;
+            int stopValue = 7;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 164.845;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+
---
+++ Tyuiu.RagozinaAD.Sprint3.Task1.V13/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.RagozinaAD.Sprint3.Task1.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл while,                                *");
+        Console.WriteLine("* которая вычисляет произведение ряда по формуле, при n=1,2               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double value = 1.2;
+        int startValue = 1;
+        int stopValue = 7;
+
+        Console.WriteLine("Переменная n = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}  
+
