f657e645aeebf1eac2c29372a08688247b911306
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-13 20:05:27 +0500

    Add project files.

---
+++ Tyuiu.RagozinaAD.Sprint3.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RagozinaAD.Sprint3.Task0.V27.Lib;
+namespace Tyuiu.RagozinaAD.Sprint3.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 13;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 16.016;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+
---
+++ Tyuiu.RagozinaAD.Sprint3.Task0.V27/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.RagozinaAD.Sprint3.Task0.V27.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл for,                                  *");
+        Console.WriteLine("* которая вычисляет сумму ряда по формуле, при n=5                        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int value = 5;
+        int startValue = 1;
+        int stopValue = 13;
+
+        Console.WriteLine("Переменная n = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма = " + ds.GetSumSeries(value, startValue, stopValue));
+        Console.ReadKey();
+
+    }
+}
